bba80a1589ee0daf5b946a1f117702b9d678518a
ryugahee <gagml910@hanmail.net>
2023-11-03 06:28:59 +0900

    환경 세팅

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <div class="album py-5 bg-body-tertiary">
+      <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <div class="col" v-for="i in 12" :key="i">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "@/components/Card.vue";
+
+export default {
+  name: 'Home',
+  components: {Card},
+}
+</script>
+
+<style scoped>
+
+</style>
